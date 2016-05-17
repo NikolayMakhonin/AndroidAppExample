a4b5ed9822366c9069a9afdbaf962eeca494afe9
nikolaymakhonin <nikolay.makhonin@gmail.com>
2016-05-17 15:16:33 +0700

    AAE-3 Implement Navigation View. Added header and menu item billets in Navigation View.

--- AndroidAppExample/AppExample/src/main/java/com/github/nikolaymakhonin/android_app_example/MainActivity.java
+++ AndroidAppExample/AppExample/src/main/java/com/github/nikolaymakhonin/android_app_example/MainActivity.java
@@ -1,19 +1,23 @@
 package com.github.nikolaymakhonin.android_app_example;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar _toolbar;
+    private DrawerLayout _drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initControls();
     }
@@ -21,14 +25,21 @@ public class MainActivity extends AppCompatActivity {
     //region Init controls
 
     private void initControls() {
+        _drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
         _toolbar = (Toolbar) findViewById(R.id.toolbar);
         _toolbar.setTitle(R.string.app_name);
         _toolbar.setOnMenuItemClickListener(menuItem -> false);
-        _toolbar.inflateMenu(R.menu.toolbar);
+        _toolbar.inflateMenu(R.menu.menu_toolbar);
+    }
+
+    private void initNavigationView() {
+
     }
 
     //endregion
