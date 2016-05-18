61a640fb5ed73151fa99fb7ca79d84a29351fc55
nikolaymakhonin <nikolay.makhonin@gmail.com>
2016-05-18 16:01:42 +0700

    Implement navigation view

--- AndroidAppExample/AppExample/src/main/java/com/github/nikolaymakhonin/android_app_example/MainActivity.java
+++ AndroidAppExample/AppExample/src/main/java/com/github/nikolaymakhonin/android_app_example/MainActivity.java
@@ -2,6 +2,7 @@ package com.github.nikolaymakhonin.android_app_example;
 
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,6 +11,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.github.nikolaymakhonin.android_app_example.adapters.TabsFragmentAdapter;
+
+import rebus.header.view.HeaderCompactView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,31 +55,17 @@ public class MainActivity extends AppCompatActivity {
         _toolbar.inflateMenu(R.menu.menu_toolbar);
     }
 
-    private void initActionBarToggle() {ActionBarDrawerToggle actionBarToggle = new ActionBarDrawerToggle(
-        this,
-        _drawerLayout,
-        _toolbar,
-        R.string.action_bar_menu_open,
-        R.string.action_bar_menu_close);
+    private void initActionBarToggle() {
+        ActionBarDrawerToggle actionBarToggle = new ActionBarDrawerToggle(
+            this,
+            _drawerLayout,
+            _toolbar,
+            R.string.action_bar_menu_open,
+            R.string.action_bar_menu_close);
 
         _drawerLayout.addDrawerListener(actionBarToggle);
 
         actionBarToggle.syncState();
-    }
-
-    private void initNavigationView() {
-
-        _navigationView.setNavigationItemSelectedListener(menuItem -> {
-            _drawerLayout.closeDrawers();
-
-            switch (menuItem.getItemId()) {
-                case R.id.menu_navigation_item1:
-                    //TODO: Implement menu item action
-                    break;
-            }
-
-            return true;
-        });
     }
 
     private void initTabPager() {
@@ -84,6 +73,32 @@ public class MainActivity extends AppCompatActivity {
         _viewPager.setAdapter(tabPageAdapter);
 
         _tabLayout.setupWithViewPager(_viewPager);
+    }
+
+    private void initNavigationView() {
+
+        HeaderCompactView headerCompactView = new HeaderCompactView(this, true);
+        headerCompactView.background().setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.primaryDark));
+        headerCompactView.background().setImageResource(R.drawable.navigation_header_background_6);
+        headerCompactView.avatar().setImageResource(R.drawable.navigation_header_icon);
+        headerCompactView.username("Material Design");
+        headerCompactView.email("nikolay.makhonin@gmail.com");
+        headerCompactView.setOnHeaderClickListener(view ->
+            _drawerLayout.closeDrawers()
+        );
+        _navigationView.addHeaderView(headerCompactView);
+
+        _navigationView.setNavigationItemSelectedListener(menuItem -> {
+//            _drawerLayout.closeDrawers();
+
+//            switch (menuItem.getItemId()) {
+//                case R.id.menu_navigation_item1:
+//                    //TODO: Implement menu item action
+//                    break;
+//            }
+
+            return true;
+        });
     }
 
     //endregion
